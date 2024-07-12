8f53eea11f60c140296c51b9db4b8459511e1248
Eric Mikulin <eric@picovoice.ai>
2024-07-12 16:05:03 -0700

    Initial ntfy.sh backend

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate procfs;
 extern crate reqwest;
 extern crate simplelog;
 
-use clap::{App, AppSettings, Arg};
+use clap::{arg, command};
 use log::*;
 use simplelog::*;
 use std::collections::HashMap;
@@ -68,16 +68,11 @@ impl Message {
     }
 }
 
-fn send_notification(message: &str, apikey: &str) {
-    let webhook_url = format!(
-        "https://maker.ifttt.com/trigger/command_exited/with/key/{}",
-        apikey
-    );
-    let mut webhook_payload = HashMap::new();
-    webhook_payload.insert("value1", message);
+fn send_notification(message: &str, topic: &str) {
+    let publish_url = format!("https://ntfy.sh/{}", topic);
 
     let client = reqwest::blocking::Client::new();
-    let _ = client.post(&webhook_url).json(&webhook_payload).send();
+    let _ = client.post(&webhook_url).body(message).send();
 }
 
 fn generate_settings_from_matches(matches: &clap::ArgMatches) -> config::Config {
@@ -93,13 +88,13 @@ fn generate_settings_from_matches(matches: &clap::ArgMatches) -> config::Config 
             .unwrap();
     }
 
-    if let Some(apikey) = matches.value_of("apikey") {
-        settings.set("apikey", apikey).unwrap();
+    if let Some(topic) = matches.value_of("topic") {
+        settings.set("topic", topic).unwrap();
     }
 
-    if let Some(command_arguments) = matches.values_of("command") {
+    if let Some(command_arguments) = matches.values_of("cmd") {
         settings
-            .set("command", command_arguments.collect::<Vec<&str>>())
+            .set("cmd", command_arguments.collect::<Vec<&str>>())
             .unwrap();
     }
 
@@ -113,7 +108,7 @@ fn generate_settings_from_matches(matches: &clap::ArgMatches) -> config::Config 
 
 fn setup_logging() {
     let _ = CombinedLogger::init(vec![
-        TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
+        TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
         #[cfg(debug_assertions)]
         WriteLogger::new(
             LevelFilter::Debug,
@@ -128,80 +123,53 @@ fn setup_logging() {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup_logging();
 
-    let matches = App::new("Hark!")
-        .setting(AppSettings::TrailingVarArg)
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Sets a custom config file")
-                .default_value("/etc/hark.toml")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("apikey")
-                .short("k")
-                .long("key")
-                .value_name("APIKEY")
-                .help("IFTTT API Key")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("process")
-                .short("p")
-                .long("process")
-                .value_name("PID")
-                .help("PID of the process to monitor")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("command")
-                .value_name("CMD")
-                .help("Command and arguments to be run")
-                .multiple(true)
-                .index(1),
-        )
+    let matches = command!()
+        .arg(arg!(-v --verbose "Print verbose output"))
+        .arg(arg!(-t --topic <TOPIC> "ntfy.sh topic to publish notification to"))
+        .arg(arg!(-c --config <FILE> "Sets a custom config file path").default_value("/etc/hark.toml"))
+        .arg(arg!(-s --save "Save command line settings to config file"))
+        .arg(arg!(-p --process <PID> "PID of the process to monitor"))
+        .arg(arg!(<CMD> ... "Command and arguments to be run").trailing_var_arg(true))
         .get_matches();
 
     let settings = generate_settings_from_matches(&matches);
 
-    let mut message = Message::new();
-    if let Ok(command_args) = settings.get_array("command") {
-        let base_cmd = command_args[0].clone().into_str()?;
-        message.command = Some(base_cmd.clone());
+    // let mut message = Message::new();
+    // if let Ok(command_args) = settings.get_array("command") {
+    //     let base_cmd = command_args[0].clone().into_str()?;
+    //     message.command = Some(base_cmd.clone());
 
-        let mut command = Command::new(base_cmd);
-        for arg in command_args.iter().skip(1) {
-            command.arg(arg.clone().into_str()?);
-        }
+    //     let mut command = Command::new(base_cmd);
+    //     for arg in command_args.iter().skip(1) {
+    //         command.arg(arg.clone().into_str()?);
+    //     }
 
-        let command_start = Instant::now();
-        let mut child = command.spawn().expect("Command failed to start");
+    //     let command_start = Instant::now();
+    //     let mut child = command.spawn().expect("Command failed to start");
 
-        let result = child.wait()?;
-        let command_time_elapsed = command_start.elapsed();
+    //     let result = child.wait()?;
+    //     let command_time_elapsed = command_start.elapsed();
 
-        message.exit_status = result.code();
-        message.time_elapsed = Some(command_time_elapsed);
-    } else if let Ok(processid) = settings.get_int("process") {
-        message.process = Some(processid as i32);
-        let process = procfs::process::Process::new(processid as i32)?;
+    //     message.exit_status = result.code();
+    //     message.time_elapsed = Some(command_time_elapsed);
+    // } else if let Ok(processid) = settings.get_int("process") {
+    //     message.process = Some(processid as i32);
+    //     let process = procfs::process::Process::new(processid as i32)?;
 
-        if let Ok(cmdline) = process.cmdline() {
-            message.command = Some(cmdline[0].clone());
-        }
+    //     if let Ok(cmdline) = process.cmdline() {
+    //         message.command = Some(cmdline[0].clone());
+    //     }
 
-        while process.is_alive() {
-            thread::sleep(Duration::from_secs(1));
-        }
-    }
+    //     while process.is_alive() {
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // }
 
-    if let Ok(apikey) = settings.get_str("apikey") {
-        send_notification(&message.generate(), &apikey);
-    } else {
-        error!("Unable to send notification: no APIKEY given!");
-    }
+    // if let Ok(apikey) = settings.get_str("apikey") {
+    //     send_notification(&message.generate(), &apikey);
+    // } else {
+    //     error!("Unable to send notification: no APIKEY given!");
+    // }
 
     return Ok(());
 }
